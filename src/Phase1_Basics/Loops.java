cd6fd545d034d7a48eb7cdf3d1deac696e59c9e0
sheeraz-H <xherazusain@gmail.com>
2026-03-07 01:19:32 +0500

    Add Loops class to demonstrate basic loop functionality

---
+++ src/Phase1_Basics/Loops.java
@@ -0,0 +1,12 @@
+package src.Phase1_Basics;
+
+// Loops (Repeating Tasks)
+
+public class Loops {
+    public static void main(String[] args) {
+        // print from 0 to 8
+        for (int i = 0; i < 9; i++) {
+            System.out.println("Count: " + i);
+        }
+    }
+}
