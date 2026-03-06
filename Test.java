0ef1b9f6d69742dddb6da6a9fac11af34060fb31
sheeraz-H <xherazusain@gmail.com>
2026-03-07 00:54:19 +0500

    Add variables and print statements for age, height, name, student status, and greeting

--- Test.java
+++ Test.java
@@ -1,5 +1,17 @@
 public class Test {
     public static void main(String[] args) {
+
+        int age = 25;
+        double height = 5.9;
+        String name = "John Doe";
+        boolean isStudent = true;
+        String greeting = "Hello, " + name + "!";
+
         System.out.println("Hello World!");
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Name: " + name);
+        System.out.println("Is Student: " + isStudent);
+        System.out.println("Greeting: " + greeting);
     }
 }
